de1154ddb2f0172d052d3a95d06cb734e62894d2
mrsaurabhtanwar <saurabhtanwar7320@gmail.com>
2026-07-09 22:00:21 +0530

    adding sqlalchemy to database and also adding deploy.yml

--- lassolr.py
+++ lassolr.py
@@ -1,8 +1,42 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
-import numpy as np
+from datetime import datetime
 import joblib
+import time
 
+from sqlalchemy import create_engine, String, Integer, Float, Column, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+
+engine = create_engine("sqlite:///fwi-prediction.db", connect_args={"check_same_thread":False})
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+class FWI(Base):
+    __tablename__ = "fwi-prediction-table"
+    id = Column(Integer, primary_key=True)
+    temperature = Column(Float, nullable=False)
+    rh = Column(Float, nullable=False)
+    ws = Column(Float, nullable=False)
+    rain = Column(Float, nullable=False)
+    ffmc = Column(Float, nullable=False)
+    dc = Column(Float, nullable=False)
+    isi = Column(Float, nullable=False)
+    bui = Column(Float, nullable=False)
+    fire = Column(Boolean, nullable=False)
+    pred_fwi = Column(Float, nullable=False)
+    datetime = Column(DateTime, nullable=False)
+    latency = Column(Float, nullable=False)
+    error = Column(String)
+
+Base.metadata.create_all(engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI(title="LassoCV Regression")
 
@@ -27,7 +61,9 @@ except Exception as e:
     raise RuntimeError(f"Failed to load the model: {e}")
 
 @app.post("/prediction")
-def prediction(data: UserInput):
+def prediction(data: UserInput, db: Session = Depends(get_db)):
+    current_dt = datetime.now()
+    start_time = time.time()
     try:
         scaled_input = scaler.transform([[
             data.temperature,
@@ -42,8 +78,47 @@ def prediction(data: UserInput):
         ]])
         Prediction = model.predict(scaled_input)
         predicted_fwi = Prediction[0]
+        latency = time.time() - start_time
+
+        db.add(
+            FWI(
+                temperature = data.temperature,
+                rh = data.rh,
+                ws = data.ws,
+                rain = data.rain,
+                ffmc = data.ffmc,
+                dc = data.dc,
+                isi = data.isi,
+                bui = data.bui,
+                fire = data.not_fire,
+                pred_fwi = predicted_fwi,
+                datetime = current_dt,
+                latency = latency,
+                error = None
+            )
+        )
+        db.commit()
         return f"Predicted FWI is : {predicted_fwi:.2f}"
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Predition failed: {e}")
+        db.rollback()
+        db.add(
+            FWI(
+                temperature = data.temperature,
+                rh = data.rh,
+                ws = data.ws,
+                rain = data.rain,
+                ffmc = data.ffmc,
+                dc = data.dc,
+                isi = data.isi,
+                bui = data.bui,
+                fire = data.not_fire,
+                pred_fwi = 0,
+                datetime = current_dt,
+                latency = 0,
+                error = str(e)
+            )
+        )
+        db.commit()
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {e}")
 
     
